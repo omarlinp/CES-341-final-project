8fc85f629b00ad1d2e19357c9bdab5489799e126
omarlinp <omarlinp@gmail.com>
2026-05-23 18:18:09 -0400

    finished the connection to the database

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const dotnev = require('dotenv');
 const express = require('express');
-const mongodb = require('mongodb');
+const mongodb = require('./db/mongodb'); // Import the MongoDB connection module
 
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
+
+
+
+mongodb.connectToDatabase((err) => {
+    if (err) {
+        console.error('Failed to connect to database', err);
+    } else {
+        console.log('Connected to database');
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }
+});
