68181c6cf3ff37d7dbe18fff21ae9c0d3794d532
omarlinp <omarlinp@gmail.com>
2026-05-23 18:35:58 -0400

    created the routes for the project

--- controller/routes.js
+++ controller/routes.js
@@ -1,2 +1,14 @@
 const router = require('express').Router();
+const model = require('../model');
 
+router.get('/games', model.getGamesData);
+router.get('/games/:id', model.getGameById);
+router.post('/games', model.createGame);
+router.put('/games/:id', model.updateGame);
+router.delete('/games/:id', model.deleteGame);
+
+router.get('/users', model.getUsersData);
+router.get('/users/:id', model.getUserById);
+router.post('/users', model.createUser);
+router.put('/users/:id', model.updateUser);
+router.delete('/users/:id', model.deleteUser);
