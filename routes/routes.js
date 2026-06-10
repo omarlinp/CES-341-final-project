e37f73d2ed9cc9f37bf21bb0239fb520f3fae4f4
omarlinp <omarlinp@gmail.com>
2026-06-10 09:47:04 -0400

    added the validatorjs dependency.  and finish adding validation to the project

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const model = require('../model');
+const validator = require('../middleware/validator');
 
 router.get('/games', model.getGamesData);
 router.get('/games/:id', model.getGameById);
-router.post('/games', model.createGame);
-router.put('/games/:id', model.updateGame);
+router.post('/games', validator.validateGame, model.createGame);
+router.put('/games/:id', validator.validateGame, model.updateGame);
 router.delete('/games/:id', model.deleteGame);
 
 router.get('/users', model.getUsersData);
 router.get('/users/:id', model.getUserById);
-router.post('/users', model.createUser);
-router.put('/users/:id', model.updateUser);
+router.post('/users', validator.validateUser, model.createUser);
+router.put('/users/:id', validator.validateUser, model.updateUser);
 router.delete('/users/:id', model.deleteUser);
 
 router.get('/', (req, res) => {
