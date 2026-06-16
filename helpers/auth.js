9c17734b55f871d54ce599ea98df3a4fbf8bb23a
omarlinp <omarlinp@gmail.com>
2026-06-16 10:36:18 -0400

    updated the callback adddress

--- helpers/auth.js
+++ helpers/auth.js
@@ -9,7 +9,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:5555/login/google/callback',
+        callbackURL: 'https://ces-341-final-project.onrender.com/login/google/callback',
         passReqToCallback: true
     },
     async function(request, accessToken, refreshToken, profile, done) {
